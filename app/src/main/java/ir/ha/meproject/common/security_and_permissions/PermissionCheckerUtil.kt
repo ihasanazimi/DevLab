5fce6633a3b18452934c3b43e5688663f9f9b63d
HasanAzimi <ihasan.azimi@gamil.com>
2025-04-19 13:11:06 +0330

    temp

--- app/src/main/java/ir/ha/meproject/common/security_and_permissions/PermissionCheckerUtil.kt
+++ app/src/main/java/ir/ha/meproject/common/security_and_permissions/PermissionCheckerUtil.kt
@@ -5,43 +5,61 @@ import android.content.pm.PackageManager
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import ir.ha.meproject.common.extensions.isMarshmallowPlus
 
 const val PERMISSION_REQUEST_CODE = 10010
 
 
-fun checkPermission(activity: Activity, permission: String?): Boolean {
-    return if (isMarshmallowPlus()) {
-        activity.checkSelfPermission(permission!!) == PackageManager.PERMISSION_GRANTED
+fun Activity.isPermissionGranted(permission: String): Boolean {
+    return ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED
+}
+
+fun Activity.shouldShowRationale(permission: String): Boolean {
+    return ActivityCompat.shouldShowRequestPermissionRationale(this, permission)
+}
+
+
+fun Activity.askPermission(
+    permission: String,
+    requestCode: Int = PERMISSION_REQUEST_CODE,
+    onPermissionAlreadyGranted: () -> Unit = {},
+    onShowRationale: () -> Unit = {},
+    onRequest: () -> Unit = {}
+) {
+    if (isPermissionGranted(permission)) {
+        onPermissionAlreadyGranted()
     } else {
-        activity.packageManager.checkPermission(
-            permission!!,
-            activity.packageName
-        ) == PackageManager.PERMISSION_GRANTED
-    }
-}
-
-fun requestPermission(activity: Activity, permission: String) {
-    if (isMarshmallowPlus()) { activity.requestPermissions(arrayOf(permission), PERMISSION_REQUEST_CODE) }
-} // You can add more methods as needed for handling permission results, etc.
-
-
-fun Activity.requestPermission(permission: String, requestCode: Int = PERMISSION_REQUEST_CODE) {
-    if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
-        ActivityCompat.requestPermissions(this, arrayOf(permission), requestCode)
+        if (shouldShowRationale(permission)) {
+            // کاربر رد کرده قبلاً، باید براش توضیح بدی
+            onShowRationale()
+        } else {
+            // مستقیم درخواست بده
+            ActivityCompat.requestPermissions(this, arrayOf(permission), requestCode)
+            onRequest()
+        }
     }
 }
 
 
-
-fun checkPermission(activity: Activity , permission: String, requestCode: Int) {
-    if (ContextCompat.checkSelfPermission(activity, permission) == PackageManager.PERMISSION_DENIED) {
-        // Requesting the permission
-        ActivityCompat.requestPermissions(activity, arrayOf(permission), requestCode)
-    } else {
-        Toast.makeText(activity, "Permission already granted", Toast.LENGTH_SHORT).show()
-    }
-}
+/** result
+ *
+ *
+ *override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+ *     when (requestCode) {
+ *         PERMISSION_REQUEST_CODE -> {
+ *             if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+ *                 // Permission granted, handle accordingly
+ *             } else {
+ *                 // Permission denied, handle accordingly
+ *             }
+ *             return
+ *         }
+ *         // Handle other permission request codes if needed
+ *     }
+ *     super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+ * }
+ *
+ *
+ * */
 
 
 /** result
