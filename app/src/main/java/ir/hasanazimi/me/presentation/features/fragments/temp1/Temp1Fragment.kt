ce1d07c5cbf3ea42753a7feef97488273b9363bf
HasanAzimi <ihasan.azimi@gamil.com>
2025-04-26 11:37:52 +0330

    add dynamically diagram testing

--- app/src/main/java/ir/hasanazimi/me/presentation/features/fragments/temp1/Temp1Fragment.kt
+++ app/src/main/java/ir/hasanazimi/me/presentation/features/fragments/temp1/Temp1Fragment.kt
@@ -1,9 +1,12 @@
 package ir.hasanazimi.me.presentation.features.fragments.temp1
 
-import android.graphics.Color
 import android.view.animation.LinearInterpolator
+import androidx.core.graphics.toColorInt
 import ir.hasanazimi.me.R
 import ir.hasanazimi.me.common.base.BaseFragment
+import ir.hasanazimi.me.common.extensions.getAmountFormatBySeparator
+import ir.hasanazimi.me.common.extensions.showToast
+import ir.hasanazimi.me.common.extensions.singleClick
 import ir.hasanazimi.me.common.views.segment_chart_view.SegmentChartView
 import ir.hasanazimi.me.databinding.FragmentTemp1Binding
 
@@ -15,12 +18,7 @@ class Temp1Fragment : BaseFragment<FragmentTemp1Binding>(FragmentTemp1Binding::i
         val segmentChart = requireActivity().findViewById<SegmentChartView>(R.id.segmentChart)
         segmentChart.setSegments(
             listOf(
-                SegmentChartView.Segment(21.0, Color.MAGENTA),
-                SegmentChartView.Segment(50.0, Color.CYAN),
-                SegmentChartView.Segment(10.0, Color.RED),
-                SegmentChartView.Segment(20.0, Color.GRAY),
-                SegmentChartView.Segment(35.0, Color.LTGRAY),
-                SegmentChartView.Segment(41.0, Color.BLUE),
+                SegmentChartView.Segment(1000.0,"#3F51B5"),
             )
         )
 
@@ -46,6 +44,41 @@ class Temp1Fragment : BaseFragment<FragmentTemp1Binding>(FragmentTemp1Binding::i
 
     override fun listeners() {
         super.listeners()
+
+
+        binding.add.singleClick {
+            val valueNumber = binding.input.text.toString()
+            if (valueNumber.isNotEmpty()){
+                val newColor = getRandomColorHex()
+                binding.add.setBackgroundColor(newColor.toColorInt() )
+                binding.segmentChart.addNewSegment(
+                    SegmentChartView.Segment(
+                        value = valueNumber.toDouble(),
+                        color = newColor
+                    )
+                )
+                binding.cashBackAmount.text = binding.segmentChart.getSumOfSegmentsValue().toString().getAmountFormatBySeparator()
+            }else{
+                showToast(requireActivity(),"please enter the new value of NewSegment")
+            }
+        }
+
+
+        binding.clearSegments.singleClick {
+            binding.segmentChart.clearAllSegments()
+            binding.cashBackAmount.text = "0"
+            binding.input.text?.clear()
+        }
+    }
+
+
+    fun getRandomColorHex(): String {
+        val random = java.util.Random()
+        val color = String.format(
+            "#%06X",
+            (0xFFFFFF and random.nextInt(0xFFFFFF + 1))
+        )
+        return color
     }
 
 }
